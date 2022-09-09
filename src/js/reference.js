056ed078a398789fe4df1df327b60cb59ade045b
Ivan Prodeus <learning.webpractice@gmail.com>
2022-09-10 00:04:00 +0300

    added files and began to do the task

---
+++ src/js/reference.js
@@ -0,0 +1,13 @@
+const LINK_TO_BACK = 'https://pixabay.com/api/?key=';
+const ACCESS_KEY = '29824999-ac7732ff734cbf874c6efba54';
+const REQUEST_PARAMETERS = 'image_type=photo&orientation=horizontal&safesearch=true'
+//вигляд запиту до бекєнду `${LINK_TO_BACK}${ACCESS_KEY}&q=${вводиться в input}&${REQUEST_PARAMETERS}&page=1&per_page=20`
+
+const refs = {
+    body: document.querySelector('body'),
+    form: document.querySelector('.search-form'),
+    input: document.querySelector('[name="serchQuery"]'),
+    button: document.querySelector('[type="submit"]')
+};
+
+export {LINK_TO_BACK, ACCESS_KEY, REQUEST_PARAMETERS, refs}
